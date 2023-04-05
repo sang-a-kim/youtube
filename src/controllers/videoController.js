56598ab4d32e692ee400db8897a4a0d716ee1a8e
김상아 Sang-A Kim <98872193+sang-a-kim@users.noreply.github.com>
2023-04-05 17:29:58 +0900

    feat: replace middleware with static

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -34,7 +34,7 @@ export const postEdit = async (req, res) => {
 	await Video.findByIdAndUpdate(id, {
 		title,
 		description,
-		hashtags,
+		hashtags: Video.formatHashtags(hashtags),
 	});
 	return res.redirect(`../${id}`);
 };
@@ -47,7 +47,7 @@ export const postUpload = async (req, res) => {
 		await Video.create({
 			title,
 			description,
-			hashtags,
+			hashtags: Video.formatHashtags(hashtags),
 		});
 		return res.redirect("/");
 	} catch (e) {
