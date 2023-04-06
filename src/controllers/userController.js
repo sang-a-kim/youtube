ccd90b6228b82e842de8da6b4ebaebbf5d083650
김상아 Sang-A Kim <98872193+sang-a-kim@users.noreply.github.com>
2023-04-06 17:14:59 +0900

    feat: send status code

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -6,7 +6,7 @@ export const postJoin = async (req, res) => {
 	const pageTitle = "Join";
 
 	if (password !== password2) {
-		return res.render("join", {
+		return res.status(400).render("join", {
 			pageTitle,
 			errorMessage: "Password confirmation does not match",
 		});
@@ -16,7 +16,7 @@ export const postJoin = async (req, res) => {
 		$or: [{ username }, { email }],
 	});
 	if (exists) {
-		return res.render("join", {
+		return res.status(400).render("join", {
 			pageTitle,
 			errorMessage: "This username/email is already taken",
 		});
