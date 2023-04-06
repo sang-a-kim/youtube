06d0ce61a4cc30c2b81c82d5970173006d1c42a0
김상아 Sang-A Kim <98872193+sang-a-kim@users.noreply.github.com>
2023-04-06 14:18:04 +0900

    feat: add search page

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,7 +1,7 @@
 import Video from "../models/Video";
 
 export const home = async (req, res) => {
-	const videos = await Video.find({});
+	const videos = await Video.find({}, null, { sort: { createdAt: "desc" } });
 	return res.render("home", { pageTitle: "Home", videos });
 };
 
@@ -63,4 +63,11 @@ export const deleteVideo = async (req, res) => {
 	console.log({ id });
 	await Video.findByIdAndDelete(id);
 	return res.redirect("/");
+};
+
+export const search = (req, res) => {
+	const { keyword } = req.query;
+	if (keyword) {
+	}
+	return res.render("search", { pageTitle: "search" });
 };
