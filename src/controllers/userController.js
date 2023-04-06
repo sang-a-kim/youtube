2a35317b3873d8f9151ad252112a269caea58257
김상아 Sang-A Kim <98872193+sang-a-kim@users.noreply.github.com>
2023-04-06 16:38:30 +0900

    feat: add join

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,4 +1,21 @@
-export const join = (req, res) => res.send("Join");
+import User from "../models/User";
+
+export const getJoin = (req, res) => res.render("join", { pageTitle: "Join" });
+export const postJoin = async (req, res) => {
+	const { name, email, username, password, location } = req.body;
+	try {
+		await User.create({
+			name,
+			username,
+			email,
+			password,
+			location,
+		});
+	} catch (e) {
+		console.log(e);
+	}
+	res.redirect('/');
+};
 export const edit = (req, res) => res.send("Edit User");
 export const remove = (req, res) => res.send("Remove User");
 export const login = (req, res) => res.send("Login");
--- src/server.js
+++ src/server.js
@@ -1,6 +1,6 @@
 import express from "express";
 import morgan from "morgan";
-import globalRouter from "./routers/globalRouter";
+import rootRouter from "./routers/rootRouter";
 import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
 
@@ -11,7 +11,7 @@ app.set("views", process.cwd() + "/src/views");
 app.set("view engine", "pug");
 app.use(logger);
 app.use(express.urlencoded());
-app.use("/", globalRouter);
+app.use("/", rootRouter);
 app.use("/users", userRouter);
 app.use("/videos", videoRouter);
 
