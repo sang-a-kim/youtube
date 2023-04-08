e4bd943bc0a482a22fe8fc18d2bfaa68a19f5fda
김상아 Sang-A Kim <98872193+sang-a-kim@users.noreply.github.com>
2023-04-08 17:26:37 +0900

    feat: add github login controllers

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -44,7 +44,7 @@ export const getLogin = (req, res) => {
 	return res.render("login", { pageTitle: "Login" });
 };
 export const postLogin = async (req, res) => {
-	const pageTitle = 'Login'
+	const pageTitle = "Login";
 	const { username, password } = req.body;
 	const user = await User.findOne({ username });
 	if (!user) {
@@ -55,7 +55,7 @@ export const postLogin = async (req, res) => {
 	}
 
 	const match = await bcrypt.compare(password, user.password);
-	if (!match){
+	if (!match) {
 		return res.status(400).render("login", {
 			pageTitle,
 			errorMessage: "Password is not matched.",
@@ -68,3 +68,19 @@ export const postLogin = async (req, res) => {
 };
 export const logout = (req, res) => res.send("Logout");
 export const see = (req, res) => res.send("See");
+export const startGithubLogin = (req, res) => {
+	const baseUrl = "https://github.com/login/oauth/authorize";
+	const config = {
+		client_id: "2cac5806bfe8563acc89",
+		allow_signup: false,
+		scope: "read:user user:email",
+	};
+	const params = new URLSearchParams(config);
+	const finalUrl = `${baseUrl}?${params}`;
+
+	res.redirect(finalUrl);
+};
+
+export const finishGithubLogin = (req, res) => {
+	
+}
