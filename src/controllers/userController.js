3de5fcf08baac363553cf7070221083abb0f3735
김상아 Sang-A Kim <98872193+sang-a-kim@users.noreply.github.com>
2023-04-08 12:20:48 +0900

    feat: add login

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -21,24 +21,37 @@ export const postJoin = async (req, res) => {
 			errorMessage: "This username/email is already taken",
 		});
 	}
-  try {
-    await User.create({
-      name,
-      username,
-      email,
-      password,
-      location,
-    });
-    res.redirect("/");
-  } catch (e) {
+	try {
+		await User.create({
+			name,
+			username,
+			email,
+			password,
+			location,
+		});
+		res.redirect("/");
+	} catch (e) {
 		return res.status(400).render("join", {
 			pageTitle,
 			errorMessage: e._message,
 		});
-  }
+	}
 };
 export const edit = (req, res) => res.send("Edit User");
 export const remove = (req, res) => res.send("Remove User");
-export const login = (req, res) => res.send("Login");
+export const getLogin = (req, res) => {
+	return res.render("login", { pageTitle: "Login" });
+};
+export const postLogin = async (req, res) => {
+	const { username, password } = req.body;
+	const exists = await User.exists({ username });
+	if (!exists) {
+		return res.status(400).render("login", {
+			pageTitle: "Login",
+			errorMessage: "An account with this username does not exists.",
+		});
+	}
+	res.end();
+};
 export const logout = (req, res) => res.send("Logout");
 export const see = (req, res) => res.send("See");
