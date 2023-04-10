6854f404877304390c55bdd6d688129c533b5548
김상아 Sang-A Kim <98872193+sang-a-kim@users.noreply.github.com>
2023-04-10 11:33:39 +0900

    feat: add middles wares realted to auth

--- src/middlewares.js
+++ src/middlewares.js
@@ -1,6 +1,22 @@
 export const localsMiddleware = (req, res, next) => {
-  res.locals.loggedIn = !!req.session.loggedIn;
+	res.locals.loggedIn = !!req.session.loggedIn;
 	res.locals.siteName = "Youtube";
 	res.locals.loggedInUser = req.session.user || {};
 	next();
+};
+
+export const protectorMiddleware = (req, res, next) => {
+	if (req.session.loggedIn) {
+		next();
+	} else {
+		return res.redirect("/login");
+	}
+};
+
+export const publicOnlyMiddleware = (req, res, next) => {
+	if (!req.session.loggedIn) {
+		next();
+	} else {
+		return res.redirect("/");
+	}
 };
