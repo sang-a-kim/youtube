b56802e98f40d085759ad241e0d5f3d897124def
김상아 Sang-A Kim <blossom78325@gmail.com>
2023-04-18 13:36:03 +0900

    feat: add volum control

--- src/client/js/videoPlayer.js
+++ src/client/js/videoPlayer.js
@@ -4,6 +4,10 @@ const muteBtn = document.getElementById("mute");
 const time = document.getElementById("time");
 const volumeRange = document.getElementById("volume");
 
+let volumeValue = 0.5;
+
+video.volume = volumeValue;
+
 const handlePlayClick = (e) => {
 	if (video.paused) {
 		video.play();
@@ -22,8 +26,28 @@ const handleMute = () => {
 	}
 
 	muteBtn.innerText = video.muted ? "Unmute" : "Mute";
-  volumeRange.value = video.muted ? 0 : 0.5;
+	volumeRange.value = video.muted ? 0 : volumeValue; 
+};
+
+const handleVolumeChange = (event) => {
+	const {
+		target: { value },
+	} = event;
+
+	if (video.muted) {
+		video.muted = false;
+		muteBtn.innerText = "Mute";
+	}
+
+  if (value == 0) {
+    video.muted = true;
+		muteBtn.innerText = "Unmute";
+  }
+
+	volumeValue = value;
+	video.volume = value;
 };
 
 playBtn.addEventListener("click", handlePlayClick);
 muteBtn.addEventListener("click", handleMute);
+volumeRange.addEventListener("input", handleVolumeChange);
